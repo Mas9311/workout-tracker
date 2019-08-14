6905de1c09e87ddc77235f0ac42bad92629569ef
Michael Smith <mas9311@gmail.com>
2019-08-13 21:16:14 -0600

    Incorporated file_helper.list_of_users() method into creating the values for the Combobox dropdown selections. Refactored bind into it's own method. Upon changing users, if the new selection is 'Add new User' value, change the page (DisplayWindow) to the NewUserPage. Else: save the last_user_index to the current value's index.  - This is useful for when: User selects their username (of multiple). User clicks 'Add New User'. User clicks another tab. Instead of defaulting back to the 0th index, it reverts back to the last_user_index. Created select_username method, so GUI can manually select a value. Mostly to make sure adjust_width is called.

--- src/frames/UserSelection.py
+++ src/frames/UserSelection.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from src import file_helper
 
 
 class UserSelection(ttk.Combobox):
@@ -7,18 +8,23 @@ class UserSelection(ttk.Combobox):
     def __init__(self, parent):
         self.current_user = StringVar()
         ttk.Combobox.__init__(self, parent, justify=CENTER, textvariable=self.current_user)
-        self.grid(row=0, column=1, sticky=NE)
+        self.grid(row=0, column=1, sticky='NES', pady=7)
         self.parent = parent
 
-        self.option_values = ['User1', 'User2', 'The name of this User is long for testing']
+        self.option_values = file_helper.list_of_users() + [' ➕ New User ']
+        self.last_user_index = 0
 
         self._initialize()
 
-    def _initialize(self):
+    def _bind_user_selection(self):
         self.bind('<<ComboboxSelected>>', self.change_user)
+
+    def _initialize(self):
+        self._bind_user_selection()
         self['values'] = self.option_values
-        self.current(0)  # first user is default
         self.configure(state='readonly')  # user cannot modify the name
+
+        self.current(0)  # first user is default
         self.adjust_width()
 
     def adjust_width(self):
@@ -27,12 +33,26 @@ class UserSelection(ttk.Combobox):
         new_width = min(30, max(7, len(self.get_current_user())))
         self.configure(width=new_width)
 
-    def change_user(self, event):
+    def change_user(self, event=None):
+        if self.current() is len(self.option_values) - 1:
+            # User clicked the 'Add New User' option
+            self.parent.change_page(2)
+        else:
+            self.last_user_index = self.current()
+            print('Changed to', self.get().upper())
+
         self.adjust_width()
         self.select_clear()  # clears the highlighted text
-        # for attr in dir(event.widget):
-        #     print(attr, '=>', getattr(event.widget, attr))
-        print('Changed to', self.get().upper())
 
     def get_current_user(self):
         return self.current_user.get()
+
+    def select_username(self, username=None):
+        if username is not None:
+            index = self.option_values.index(file_helper.convert_username_to_display(username))
+        else:
+            index = 0
+        print('Selecting the User Selection index of', index)
+
+        self.current(index)
+        self.adjust_width()
