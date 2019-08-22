76ff2c8e535933af899f74e21cf9802ab315b299
Michael Smith <mas9311@gmail.com>
2019-08-21 23:57:23 -0600

    Added docstrings to methods and rearranged ordering to alphabetical.

--- src/frames/TabBar.py
+++ src/frames/TabBar.py
@@ -6,7 +6,8 @@ from src import file_helper
 
 class TabBar(ttk.Notebook):
     """Creates the Notebook's tabs located at the top-left corner.
-    Allows the user to click a different tab."""
+    Allows the user to select different tabs.
+    Creates all Tabs created, but hidden Tabs are not initially displayed."""
     def __init__(self, parent):
         self.parent = parent
         self._initialize_tab_bar_style()
@@ -18,7 +19,23 @@ class TabBar(ttk.Notebook):
 
         self._create_page_tab_widgets()
 
+    def _bind_tab_bar_widget(self):
+        """Binds the left-click to TabBar's tab_clicked method"""
+        self.bind('<ButtonRelease-1>', self.tab_clicked)
+
+    def _create_page_tab_widgets(self):
+        """Creates all tabs from parent, but only shows the non-hidden tabs"""
+        for p_index, curr_tab in enumerate(self.parent.tab_info):
+            # creates 3 tabs
+            self.tabs.append(ttk.Frame(self))
+            if not curr_tab['hidden']:
+                # display 2 tabs
+                self.add(self.tabs[p_index], text=curr_tab['name'])
+
+        self._bind_tab_bar_widget()
+
     def _initialize_tab_bar_style(self):
+        """Creates the style used for the Notebook (TabBar) and the Tabs"""
         # customizes the TabBar widget (exists behind the tabs)
         self.parent.style.configure("TNotebook",
                                     background=self.parent.color('Header_Bg'),
@@ -42,41 +59,19 @@ class TabBar(ttk.Notebook):
                                     background=self.parent.color('TabBar_Inactive_Bg'),
                                     foreground=self.parent.color('TabBar_Inactive_Fg'))
 
-    def _bind_tab_bar_widget(self):
-        self.bind('<ButtonRelease-1>', self.tab_clicked)
-
-    def _create_page_tab_widgets(self):
-        for p_index, curr_tab in enumerate(self.parent.tab_info):
-            # creates 3 tabs
-            self.tabs.append(ttk.Frame(self))
-            if not curr_tab['hidden']:
-                # display 2 tabs
-                self.add(self.tabs[p_index], text=curr_tab['name'])
-
-        self._bind_tab_bar_widget()
-
     def current_tab_index(self):
+        """Returns the integer of the currently selected Tab"""
         return self.index(self.select())
 
-    def tab_clicked(self, event):
-        if self.parent.current_page_number is 2 and not file_helper.list_of_users():
-            print('Must create one user to continue')
-            self.manually_select(2)
-            self.parent.adjust_focus()
-            return
-
-        tab_index = event.widget.index('current')
-
-        print('\nTab', tab_index, 'clicked')
-        print('Tab index:', self.select())
-        self.parent.change_page(tab_index)
-
     def manually_select(self, tab_index):
+        """Manipulates the selection just as tab_clicked method's behavior """
         if self.current_tab_index() is not tab_index:
             print('Manually selecting tab #', tab_index)
             self.select(tab_index)
 
     def show_hidden_tab(self):
+        """If the user activates a hidden tab to be displayed, display it in the TabBar.
+        Manually selects the new hidden Tab."""
         tab_index = self.parent.current_page_number
         curr_tab = self.parent.tab_info[tab_index]
         if curr_tab['hidden']:
@@ -86,3 +81,16 @@ class TabBar(ttk.Notebook):
 
             # for attr in dir(event):
             #     print(attr, '=>', getattr(event, attr))
+
+    def tab_clicked(self, event):
+        """The binded method for when the user clicks a Tab in the TabBar"""
+        if self.parent.current_page_number is 2 and not file_helper.list_of_users():
+            print('Must create one user to continue')
+            self.manually_select(2)
+            self.parent.adjust_focus()
+            return
+
+        tab_index = event.widget.index('current')
+
+        print('\nTab', tab_index, 'clicked')
+        self.parent.change_page(tab_index)
