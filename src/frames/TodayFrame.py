43c09a213ff61c9ff652a15663db170905825fdc
Michael Smith <mas9311@gmail.com>
2019-08-21 23:58:25 -0600

    Created the base of how to add a new exercise frame. Added left padding to TodayFrame.

--- src/frames/TodayFrame.py
+++ src/frames/TodayFrame.py
@@ -8,29 +8,33 @@ class TodayFrame(Frame):
         self.gui = self.parent.parent
         Frame.__init__(self, self.parent.frame, background=self.gui.color('Display_Bg'))
         self.grid(row=1, column=0)
+        self.exercise_index = 1
+        self.exercises = []
 
         self.__create_today_widgets()
 
     def __create_today_widgets(self):
-        ExerciseFrame(self)
-        Button(self).grid(row=10, column=0)
+        ttk.Frame(self, width=20, style='Display_Bg.TFrame').grid(row=0, column=0)
+        self.create_exercise()
+        self.create_exercise()
+        Button(self).grid(row=10, column=1)
+
+    def create_exercise(self):
+        print(len(self.exercises), self.exercise_index)
+        self.exercises.append(ExerciseFrame(self, self.exercise_index))
+        self.exercise_index += 1
 
 
 class ExerciseFrame(Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, _row):
         self.parent = parent
         self.gui = self.parent.gui
         Frame.__init__(self, parent, background=self.gui.color('DisplayLabel_Bg'))
-        self.grid(row=0, column=0)
+        self.grid(row=_row, column=1, pady=4)
+        self._row = _row
 
         self.__create_exercise_widgets()
 
     def __create_exercise_widgets(self):
-        label = ttk.Label(self, text='workout', background=self.gui.color('DisplayLabel_Bg'))
+        label = ttk.Label(self, text=f'Test workout {self._row}', background=self.gui.color('DisplayLabel_Bg'))
         label.grid(row=0, column=0)
-        label = ttk.Label(self, text='workout')
-        label.grid(row=0, column=1)
-        label = ttk.Label(self, text='workout')
-        label.grid(row=0, column=2)
-        label = ttk.Label(self, text='workout')
-        label.grid(row=0, column=3)
